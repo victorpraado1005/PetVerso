0d6653f785a809fd832e2ea8686bf7cdd0b76229
Victor Prado <prado.victor1005@gmail.com>
2022-08-16 17:35:13 -0300

    alterando conexao do banco de dados

--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,18 @@
 const { Client } = require('pg');
 
+// const client = new Client({
+//   host: 'ec2-44-205-63-142.compute-1.amazonaws.com',
+//   port: 5432,
+//   user: 'wvxjfarfsgtslp',
+//   password: '8901b534831514e518e4ffd11385fea8b9f96f12fa39f8a1731ef0e4ac669dae',
+//   database: 'der3ai32lv1q0p'
+// });
+
 const client = new Client({
-  host: 'localhost',
-  port: 5432,
-  user: 'root',
-  password: 'root',
-  database: 'petverso',
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 client.connect();
