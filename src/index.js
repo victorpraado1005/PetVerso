a3d15a641ae3f5e3e6b60257a04f47f3dd9dc0ec
Victor Prado <prado.victor1005@gmail.com>
2022-08-17 17:09:03 -0300

    adicionando header de liberação na API

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use((request, response, next) => {
+  response.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+  next();
+})
 app.use(routes);
 //error handler
 app.use((error, request, response, next) => {
