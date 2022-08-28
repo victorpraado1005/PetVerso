b4b25dbebad0b43ea14454f08b5d3be51dfd08cb
Victor Prado <prado.victor1005@gmail.com>
2022-08-28 00:40:33 -0300

    adicionando header de access-control-allow-methods

--- src/App/middlewares/cors.js
+++ src/App/middlewares/cors.js
@@ -1,5 +1,6 @@
 module.exports = (request, response, next) => {
   response.setHeader('Access-Control-Allow-Origin', '*');
   response.setHeader('Access-Control-Allow-Headers', '*');
+  response.setHeader('Access-Control-Allow-Methods', '*');
   next();
 }
