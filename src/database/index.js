3914e29dfb61927e9ec70e9109f4d0bb72e5d9b4
Victor Prado <prado.victor1005@gmail.com>
2022-09-02 15:46:40 -0300

    ajustando conexao do banco de dados

--- src/database/index.js
+++ src/database/index.js
@@ -1,19 +1,19 @@
 const { Client } = require('pg');
 
-const client = new Client({
-  host: 'localhost',
-  port: 5432,
-  user: 'root',
-  password: 'root',
-  database: 'petverso'
-});
-
 // const client = new Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
+//   host: 'localhost',
+//   port: 5432,
+//   user: 'root',
+//   password: 'root',
+//   database: 'petverso'
 // });
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 client.connect();
 
