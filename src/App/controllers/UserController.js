ae91cf2515182598ff858921b99f6e32c1b4f10b
Victor Prado <prado.victor1005@gmail.com>
2022-10-25 18:37:00 -0300

    ajustando erro que estava quebrando a aplicação no user controllers

--- src/App/controllers/UserController.js
+++ src/App/controllers/UserController.js
@@ -1,5 +1,5 @@
-const UsersRepository = require('../repositories/UsersRepository');
-require('express-async-error');
+const UsersRepository = require("../repositories/UsersRepository");
+require("express-async-error");
 
 class UserController {
   async index(request, response) {
@@ -12,83 +12,124 @@ class UserController {
     const user = await UsersRepository.findById(id);
 
     if (!user) {
-      return response.status(404).json({error: 'User not found'})
+      return response.status(404).json({ error: "User not found" });
     }
 
     response.json(user);
   }
 
-  async login (request, response) {
+  async login(request, response) {
     const { email, password } = request.body;
-    const user = await UsersRepository.findUserByEmailAndPassword(email, password);
+    const user = await UsersRepository.findUserByEmailAndPassword(
+      email,
+      password
+    );
 
-    if (!user){
-      return response.status(404).json({ error: 'E-mail or password are incorrect' })
+    if (!user) {
+      return response
+        .status(404)
+        .json({ error: "E-mail or password are incorrect" });
     }
 
     response.json(user);
   }
 
-  async store (request, response) {
+  async store(request, response) {
     //Criar novo registro
     const {
-      name, email, phone, address, cep, city, estado, gender, assinante, password
+      name,
+      email,
+      phone,
+      address,
+      cep,
+      city,
+      estado,
+      gender,
+      assinante,
+      password,
     } = request.body;
 
     if (!name) {
-      return response.status(400).json({ error: 'Name is required!' });
+      return response.status(400).json({ error: "Name is required!" });
     }
 
-    if (!email){
-      return response.status(400).json({ error: 'Email is required!' });
+    if (!email) {
+      return response.status(400).json({ error: "Email is required!" });
     }
 
-<<<<<<< HEAD
     const emailExists = await UsersRepository.findByEmail(email);
-    if (emailExists){
-=======
-    const { emailExists } = await UsersRepository.findByEmail(email);
-    if (!emailExists){
->>>>>>> parent of 68a12c7 (ajustando repository de creaet user)
-      return response.status(400).json({ error: 'This e-mail is already in use' });
+    if (emailExists) {
+      const { emailExists } = await UsersRepository.findByEmail(email);
+      if (!emailExists) {
+        return response
+          .status(400)
+          .json({ error: "This e-mail is already in use" });
+      }
+
+      if (!phone) {
+        return response.status(400).json({ error: "Phone is required!" });
+      }
+
+      const contact = await UsersRepository.create({
+        name,
+        email,
+        phone,
+        address,
+        cep,
+        city,
+        estado,
+        gender,
+        assinante,
+        password,
+      });
+
+      response.status(201).json(contact);
     }
-
-    if (!phone){
-      return response.status(400).json({ error: 'Phone is required!' });
-    }
-
-    const contact = await UsersRepository.create({
-      name, email, phone, address, cep, city, estado, gender, assinante, password
-    });
-
-    response.status(201).json(contact);
   }
 
   async update(request, response) {
     const { id } = request.params;
     const {
-      name, email, phone, address, cep, city, estado, gender, date_of_birth, assinante
+      name,
+      email,
+      phone,
+      address,
+      cep,
+      city,
+      estado,
+      gender,
+      date_of_birth,
+      assinante,
     } = request.body;
 
     const userExists = await UsersRepository.findById(id);
     if (!userExists) {
-      return response.status(400).json({error: 'User not found'});
+      return response.status(400).json({ error: "User not found" });
     }
 
     if (!name) {
-      return response.status(400).json({ error: 'Name is required!' });
+      return response.status(400).json({ error: "Name is required!" });
     }
 
-    if (!email){
-      return response.status(400).json({ error: 'Email is required!' });
+    if (!email) {
+      return response.status(400).json({ error: "Email is required!" });
     }
 
-    if (!phone){
-      return response.status(400).json({ error: 'Phone is required!' });
+    if (!phone) {
+      return response.status(400).json({ error: "Phone is required!" });
     }
 
     const user = await UsersRepository.update(id, {
-      name, email, phone, address, cep, city, estado, gender, date_of_birth, assinante
+      name,
+      email,
+      phone,
+      address,
+      cep,
+      city,
+      estado,
+      gender,
+      date_of_birth,
+      assinante,
     });
 
     response.json(user);
